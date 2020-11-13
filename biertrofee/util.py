358b76bcbc95b271e79386d7c7c08b847bbbdbc6
Samuel5602 <samueldankers@hetnet.nl>
2020-11-13 23:35:12 +0100

    match models list

--- biertrofee/util.py
+++ biertrofee/util.py
@@ -1,9 +1,11 @@
 from .models import Player, TeamName, Poul, Team, Match
 
 def create_matches(teams):
+    i = 0
     Match.objects.all().delete()
     for team1 in teams:
         for team2 in teams:
             if team1 is not team2:
-                temp = Match(team1, team2)
+                temp = Match(home=team1, away=team2)
                 temp.save()
+                i += 1
